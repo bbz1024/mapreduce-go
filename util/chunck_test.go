abdc6ebe877b378ab9d5cc97b05746d549e615a7
bbz <2632141215@qq.com>
2024-12-16 20:30:05 +0800

    feat: mapreduce_test

--- util/chunck_test.go
+++ util/chunck_test.go
@@ -3,7 +3,7 @@ package util
 import "testing"
 
 func TestSplitChunk2Save(t *testing.T) {
-	err := SplitChunk2Save("D:\\code\\go\\mapreduce\\wordcounts.txt", "./temp", 3)
+	err := SplitChunk2Save("../wordcounts.txt", "./temp", 3)
 	if err != nil {
 		t.Error(err)
 	}
